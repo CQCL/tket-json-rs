09d78496a4256101c920ae516edd118c7e0496f0
seyon <seyon.sivarajah@cambridgequantum.com>
2022-05-18 12:01:27 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+pub mod circuit_json;
+pub mod optype;
+
+#[cfg(test)]
+mod tests {
+    use crate::circuit_json::SerialCircuit;
+    use serde_json::to_string;
+    #[test]
+    fn read_json() {
+        // let expr = symengine::Expression::new("a + b + 3");
+        let circ_s = r#"{"bits": [["c", [0]], ["c", [1]]], "commands": [{"args": [["q", [0]]], "op": {"type": "H"}}, {"args": [["q", [0]], ["q", [1]]], "op": {"type": "CX"}}, {"args": [["q", [0]], ["c", [0]]], "op": {"type": "Measure"}}, {"args": [["q", [1]], ["c", [1]]], "op": {"type": "Measure"}}], "implicit_permutation": [[["q", [0]], ["q", [0]]], [["q", [1]], ["q", [1]]]], "phase": "0.0", "qubits": [["q", [0]], ["q", [1]]]}"#;
+        let ser: SerialCircuit = serde_json::from_str(circ_s).unwrap();
+
+        assert_eq!(ser.commands.len(), 4);
+
+        let reser: SerialCircuit = serde_json::from_str(&to_string(&ser).unwrap()[..]).unwrap();
+        assert_eq!(ser, reser);
+    }
+}

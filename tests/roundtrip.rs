318263aab51d739b634b7e7cd96340282e4529bb
Agustín Borgna <agustin.borgna@quantinuum.com>
2024-11-05 14:15:50 +0000

    Normalize jsons when testing

--- tests/roundtrip.rs
+++ tests/roundtrip.rs
@@ -10,6 +10,26 @@ const DIAGONAL: &str = include_str!("data/diagonal-box.json");
 const QASM: &str = include_str!("data/qasm.json");
 const WASM: &str = include_str!("data/wasm.json");
 
+/// Cleanup some fields in the JSON so that we can compare them.
+fn normalize_json(json: &mut Value) {
+    if let Value::Object(obj) = json {
+        // Some versions of pytket include the `created_qubits` and `discarded_qubits` fields
+        // even if they are empty. Some other versions do not include them at all.
+        //
+        // We remove them here.
+        if let Some(Value::Array(registers)) = obj.get_mut("created_qubits") {
+            if registers.is_empty() {
+                obj.remove("created_qubits");
+            }
+        }
+        if let Some(Value::Array(registers)) = obj.get_mut("discarded_qubits") {
+            if registers.is_empty() {
+                obj.remove("discarded_qubits");
+            }
+        }
+    }
+}
+
 #[rstest]
 #[case::simple(SIMPLE, 4)]
 #[case::classical(CLASSICAL, 3)]
@@ -17,12 +37,14 @@ const WASM: &str = include_str!("data/wasm.json");
 #[case::qasm_box(QASM, 4)]
 #[case::wasm_box(WASM, 1)]
 fn roundtrip(#[case] json: &str, #[case] num_commands: usize) {
-    let initial_json: Value = serde_json::from_str(json).unwrap();
+    let mut initial_json: Value = serde_json::from_str(json).unwrap();
+    normalize_json(&mut initial_json);
     let ser: SerialCircuit = serde_json::from_value(initial_json.clone()).unwrap();
 
     assert_eq!(ser.commands.len(), num_commands);
 
-    let reencoded_json = serde_json::to_value(&ser).unwrap();
+    let mut reencoded_json = serde_json::to_value(&ser).unwrap();
+    normalize_json(&mut reencoded_json);
     assert_json_eq!(reencoded_json, initial_json);
 
     let reser: SerialCircuit = serde_json::from_value(reencoded_json).unwrap();
